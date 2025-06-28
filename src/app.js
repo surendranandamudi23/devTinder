38a2b884eac5e82761f1dd8dc03ed047ed334a8b
Vikram <vikram@tanstech.com>
2025-06-28 21:47:44 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -10,6 +10,67 @@ app.post("/signup", async (req, res) => {
   console.log(result);
   res.send("User created");
 });
+app.get("/user", async (req, res) => {
+  const email = req.body?.email;
+  try {
+    if (!email) {
+      res.status(400).send("Email is required");
+    }
+    const userRes = await User.find({ email });
+    if (userRes.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(userRes);
+    }
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
+});
+app.get("/feed", async (req, res) => {
+  try {
+    const userRes = await User.find({});
+    if (userRes.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(userRes);
+    }
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
+});
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    if (!userId) {
+      res.status(400).send("Please provide id");
+    }
+    const userToDelete = await User.findByIdAndDelete(userId);
+    console.log(userToDelete);
+    res.send("User Deleted Successfully");
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
+});
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const body = req.body;
+  if (!userId) {
+    res.status(400).send("Please provide id");
+  }
+  try {
+    const userUpdated = await User.findByIdAndUpdate(userId, body, {
+      runValidators: true,
+    });
+    console.log(userUpdated);
+    res.send("Updated Successfully");
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
+});
+app.use("/", (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send(err.message);
+});
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,93 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+function validatePassword(value) {
+  const errors = [];
+  if (value.length < 6) {
+    errors.push("at least 6 characters");
+  }
+  if (!/[A-Z]/.test(value)) {
+    errors.push("an uppercase letter");
+  }
+  if (!/\d/.test(value)) {
+    errors.push("a number");
+  }
+  if (!/[!@#$%^&*]/.test(value)) {
+    errors.push("a special character");
+  }
+  return errors;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      minLength: 2,
+      maxLength: 50,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 100,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender must be 'male', 'female', or 'others'.");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    },
+    about: {
+      type: String,
+      default: "This is about me",
+    },
+    skills: {
+      type: [String],
+      default: [],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  if (this.gender) {
+    this.gender = this.gender.toLowerCase().trim();
+  }
+
+  const passwordErrors = validatePassword(this.password);
+  if (passwordErrors.length > 0) {
+    const err = new Error(
+      `Password must contain ${passwordErrors.join(", ")}.`
+    );
+    return next(err);
+  }
+
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
